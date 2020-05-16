9bd2ef37f5c434416acca1a39328d2634d5faf2f
Alesandro Fidelis <alesandro.fidelis@hbsis.com.br>
2020-05-16 11:59:29 -0300

    add cors

--- APITestHeroku/Startup.cs
+++ APITestHeroku/Startup.cs
@@ -26,6 +26,7 @@ namespace APITestHeroku
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +42,12 @@ namespace APITestHeroku
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseCors(x => x
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowCredentials());
 
             app.UseEndpoints(endpoints =>
             {
